848785340af8f3114aba9de38f54e02929cad439
Yongjian Feng <yongjiaf@ah-yongjiaf-mac.dhcp.mathworks.com>
2020-04-19 21:55:12 -0400

    typo

--- CorData/app_config.m
+++ CorData/app_config.m
@@ -2,8 +2,7 @@
 app_init_dw_apikey='your_api_key_here';
 %State data to be shown (optional)
 app_init_show_state='Massachusetts';
-%Country data to show (optional) ['US', 'China', 'Japan', 'United Kingdom',
-%'France', 'Spain', 'Germany']
-app_init_show_country='China'
+%Country data to show (optional) 
+app_init_show_country='US'
 %Forced to download even if local data file exists (optional)
 app_init_always_download=false;
